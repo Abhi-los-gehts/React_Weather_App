b203977a3d1311d527fc0b8c86d48832c297a261
Abhi <abhilasha.reva61@gmail.com>
2023-11-26 19:46:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+
+      <form action="">
+
+<input type="text" name="city" placeholder='Enter Location' id="" />
+
+<button>Submit</button>
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
